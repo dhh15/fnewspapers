1472deb438315b70ca9dcf7cbba4fd74346365cd
Eric Malmi <eric.malmi@aalto.fi>
2015-05-13 12:51:38 +0000

    word clouds and baseline frequencies

--- code/baseline_freqs.py
+++ code/baseline_freqs.py
@@ -2,23 +2,38 @@ import gzip
 import cPickle as pickle
 
 x = {}
-f = gzip.open('../data/klk_fi_1grams_189x.gz')
 
+#f = gzip.open('../data/klk_fi_1grams_189x.gz')
+filename = '/srv/work/klk_fi_1grams_190x.gz'
+f = gzip.open(filename)
 i = 0
 total = 0
 for line in f:
     if i % 1000000 == 0:
         print i
     parts = line.split()
-    key = parts[1].strip().decode('utf-8').lower()
     val = int(parts[0])
     total += val
-    if len(key) >= 3:
-        x[key] = x.get(key,0) + val
     i += 1
+f.close()
+print "Total words: %d" % total
+
+f = gzip.open(filename)
+i = 0
+for line in f:
+    if i % 1000000 == 0:
+        print i
+    parts = line.split()
+    key = parts[1].strip().decode('utf-8').lower()
+    val = int(parts[0])
+    frac = float(val) / total
+    if len(key) >= 4 and frac >= 0.00000001:
+        x[key] = x.get(key,0) + frac
+    i += 1
+f.close()
 print "%d rows, %d elements in dict, %d words in total" % (i, len(x), total)
 
 print "Starting to save..."
-with open('/srv/work/unigrams3utf8.pckl','wb') as fout:
+with open('/srv/work/unigrams_1900.pckl','wb') as fout:
     pickle.dump((x,total), fout)
 print "Saved."
---
+++ code/word_clouds.py
@@ -0,0 +1,35 @@
+from os import path
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import codecs
+
+def get_frequencies(filename):
+    f = codecs.open(filename, 'r', 'utf-8')
+    x = []
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        parts = line.split(',')
+        word = parts[0]
+        freq = float(parts[1])
+        x.append((word,freq))
+    return x
+
+def draw_cloud(freqs, picture_filename, n_words=100):
+    wordcloud = WordCloud().generate_from_frequencies(freqs[:n_words])
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+    plt.savefig(picture_filename)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = '../results/keywords_2015-05-13T121302.csv'
+    if len(sys.argv) > 2:
+        picture_filename = sys.argv[2]
+    else:
+        picture_filename = '../pics/wordcloud.png'
+    freqs = get_frequencies(filename)
+    draw_cloud(freqs, picture_filename)
